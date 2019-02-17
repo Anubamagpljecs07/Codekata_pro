e796e1662bf9ba16cf6df93c7f1656e85abe0948
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-17 12:05:01 +0530

    Update Weight_equalize.py

--- Weight_equalize.py
+++ Weight_equalize.py
@@ -3,10 +3,19 @@ r=input()
 s=list(s.split("|"))
 if len(s[0])==len(s[1]):
     print("Impossible")
-elif len(s[1])+len(r)==len(s[0]):
-    g=""
-    s[1]=s[1]+r
-    g=g+s[0]+"|"+s[1]
-    print(g)
-else:
-    print("Impossible")
+if len(s[1])<len(s[0]):
+    if len(s[1])+len(r)==len(s[0]):
+        g=""
+        s[1]=s[1]+r
+        g=g+s[0]+"|"+s[1]
+        print(g)
+    else:
+        print("Impossible")
+if len(s[0])<len(s[1]):
+    if len(s[0])+len(r)==len(s[1]):
+        g=""
+        s[0]=s[0]+r
+        g=g+s[0]+"|"+s[1]
+        print(g)
+    else:
+        print("Impossible")
