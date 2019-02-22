97f82fcd26f5c24b7fb214e5b4435c654713506e
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-22 15:54:43 +0530

    Update Extreme_local.py

--- Extreme_local.py
+++ Extreme_local.py
@@ -5,6 +5,6 @@ for i in range(0,len(a)):
 	if i>0 and i<len(a)-1:
 		if a[i]<a[i+1] and a[i]<a[i-1]:
 			c+=1
-		elif a[i]>a[i+1] and a[i]>a[ai-1]:
+		elif a[i]>a[i+1] and a[i]>a[i-1]:
 			c+=1
 print(c)
