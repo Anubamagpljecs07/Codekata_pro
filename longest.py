d35492a860f2755776d55e361d5fee1983521516
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-22 16:29:35 +0530

    Create longest.py

---
+++ longest.py
@@ -0,0 +1,21 @@
+n=int(input())
+l=list(map(int,input().split()))
+g=[]
+c=1
+for i in range(0,len(l)):
+	if i<len(l)-1 and i>1:
+		if l[i]>l[i-1]:
+			c+=1
+		else:
+			g.append(c)
+			c=1
+	if i==0:
+		if l[i]<l[i+1]:
+			c+=1
+			g.append(c)
+		else:
+			g.append(c)
+a=max(g)
+if a<2:
+	a=a-2
+print(a)
