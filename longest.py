d4eb768ffd554c16bb77754943e9d10935a04d61
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-22 16:51:21 +0530

    Update longest.py

--- longest.py
+++ longest.py
@@ -3,16 +3,19 @@ l=list(map(int,input().split()))
 g=[]
 c=1
 for i in range(0,len(l)):
-	if i<len(l)-1 and i>1:
-		if l[i]>l[i-1]:
+	if i<=len(l)-1 and i>1:
+		if l[i]>=l[i-1]:
 			c+=1
-		else:
+		elif l[i]<l[i-1]:
 			g.append(c)
 			c=1
-	if i==0:
-		if l[i]<l[i+1]:
-			c+=1
+		if i==len(l)-1:
 			g.append(c)
+	if i==0:
+		if l[i]<=l[i+1]:
+			c+=1
+			if len(l)==2:
+				g.append(c)
 		else:
 			g.append(c)
 a=max(g)
