98ffae88560438492b938eab0b0d4727822f73a8
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-23 22:16:14 +0530

    Create Equal_groups.py

---
+++ Equal_groups.py
@@ -0,0 +1,23 @@
+n,a,b=map(int,input().split())
+m=n//2
+c=0
+if n%2==1:
+	print("NO")
+else:
+	for i in range(1,m+1):
+		for j in range(1,m+1):
+			if i*a==m:
+				print("YES")
+				c+=1
+				break
+			elif i*b==n:
+				print("YES")
+				c+=1
+				break
+			elif i*a + j*b==m:
+				print("YES")
+				c+=1
+				break
+		if c>=1:
+			break
+		
