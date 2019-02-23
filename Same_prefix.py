a8371ef495fcbc61441ce6bb83c952376756b97a
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-23 21:56:09 +0530

    Update Same_prefix.py

--- Same_prefix.py
+++ Same_prefix.py
@@ -6,13 +6,15 @@ for i in l:
 	m.append(len(i))
 n=min(m)
 g=l[0][0:n]
+g=list(g)
 a=""
 c=0
 for i in range(1,len(l)):
 	for j in range(0,n):
 		if c==0:
-			if g[j]==l[i][j]:
-				a=a+g[j]
-			else:
-				break
+			if g[j]!=l[i][j]:
+				g[j]=""
+			
+for i in g:
+	a=a+i
 print(a)
