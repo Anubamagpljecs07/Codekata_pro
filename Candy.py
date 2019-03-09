58cdb202373ed2596cd51b82e6edaadc54552ccf
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-03-09 16:08:53 +0530

    Create Candy.py

---
+++ Candy.py
@@ -0,0 +1,17 @@
+n=int(input())
+l=list(map(int,input().split()))
+g=[]
+for j in range(n):
+	if len(l)!=0:
+		m=min(l)
+		a=l.count(m)
+		g.append(a)
+		for i in range(0,a):
+			l.remove(m)
+f=0
+j=1
+for i in g:
+	f=f+(i*j)
+	j+=1
+print(f)
+	
